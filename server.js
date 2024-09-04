2d186cafcef743c7cdccdb409413271b12642efc
Gulshanverma7878 <144835066+Gulshanverma7878@users.noreply.github.com>
2024-09-04 16:32:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.use('/api/birth', BirthToday);
 app.use('/api/notification', NotificationRoute);
 
 app.get('/', (req, res) => {
-  res.send('API RUNNING.........');
+  res.send('mona fix it.........');
 });
 
 app.use((err, req, res, next) => {
